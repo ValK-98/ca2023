ffd02ae78105663bb928e92330deb6109d03a08c
ValK-98 <vkornijenka@gmail.com>
2023-10-09 12:34:54 +1000

    notes for function example

--- python/functions/functions.py
+++ python/functions/functions.py
@@ -1,3 +1,13 @@
-def hello():
-    print('Hello') 
-    print('Goodbye!')
+# def hello():
+#     print('Hello') 
+#     print('Goodbye!')
+
+def add_gst(amount):
+        total = amount * 1.1
+        return total
+
+
+subtotal = float(input('Subtotal: $'))
+grand_total = add_gst(subtotal)
+print(f'Total: ${grand_total: .2f}') 
+
