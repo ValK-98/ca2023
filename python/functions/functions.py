8069d6cb0d55aedfd2928acec9120d103ddd4577
ValK-98 <vkornijenka@gmail.com>
2023-10-09 13:04:05 +1000

    new notes for functions

--- python/functions/functions.py
+++ python/functions/functions.py
@@ -2,12 +2,21 @@
 #     print('Hello') 
 #     print('Goodbye!')
 
-def add_gst(amount):
-        total = amount * 1.1
-        return total
+FLAT_SHIPPING: 10
+TAX_RATE = 0.1
 
+def add_tax(amount):
+        return amount * (1 + TAX_RATE)
+
+def add_shipping(amount):
+        return amount + FLAT_SHIPPING
+
+def calc_grant_total(amount):
+        return add_tax(add_shipping(amount))
+
+#Main
 
 subtotal = float(input('Subtotal: $'))
-grand_total = add_gst(subtotal)
+grand_total = calc_grant_total(subtotal)
 print(f'Total: ${grand_total: .2f}') 
 
