1e3ac20feef940293a32d8e0a11275b70fe61ddf
ValK-98 <vkornijenka@gmail.com>
2023-10-10 10:50:45 +1000

    while/for loops examples

--- python/loops-and-lists/loops.py
+++ python/loops-and-lists/loops.py
@@ -1,3 +1,10 @@
-spam -= 0 
-if spam < 5:
-    print('Hello')
+# spam = 0 
+# while spam < 5:
+#     print('Hello')
+    #   spam += 1 
+
+
+# for spam in range(5): 
+#     print(f'Hello {spam}')
+
+import random 
