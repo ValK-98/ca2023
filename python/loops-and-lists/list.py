775ccdb9d68618f9eba001835f127979d2f0d5e5
ValK-98 <vkornijenka@gmail.com>
2023-10-10 13:11:41 +1000

    list examples

--- python/loops-and-lists/list.py
+++ python/loops-and-lists/list.py
@@ -16,3 +16,25 @@ tic_tac_toe = [
 
 # print(list(range(1, 10, 2)))
 
+# or .extend(foo)
+# or spam += foo
+
+# spam.append('kangaroo')
+# print(spam)
+
+# x = spam.index('dog')
+# print(x)
+
+# def display_person(person):
+#     print(f'{person[0]} is {person[1]} years old and {person[2]} cm tall')
+
+# def display_person(person):
+#     # name = person[0]
+#     # age = person[1]
+#     # height = person[2]
+#     name, age, height = person
+#     print(f'{name} is {age} years old and {height} cm tall')
+
+
+spam.insert(1, 'kangaroo')
+print(spam)
