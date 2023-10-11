3fe3040c4b9cbc4803b5ff530d4e9c872a48ba07
ValK-98 <vkornijenka@gmail.com>
2023-10-11 11:33:27 +1000

    dictionary notes

--- python/data-structures/dicts.py
+++ python/data-structures/dicts.py
@@ -1,4 +1,20 @@
 my_dog = { 'name': 'Ted', 'age': 15, 'breed': 'Border Collie'}
 # Key value pairs. 'name' is key - 'Ted' is value.
+# A tuple is an immutable list. Has key and value in it
 
-print(my_dog)
+my_dog['age'] = 16
+# Can change values
+my_dog['owner'] = 'Matt'
+# Can add keys
+
+# print(my_dog.items())
+# Gives tuples
+
+# for k, v in my_dog.items():
+#     print(f'The value of "{k}" is {v}')
+
+# print(item) Gives keys, my_dog[item] gives values
+
+print(my_dog.get('stage', 'Unknown'))
+
+# by using the .get method it returns None rather than error. 
