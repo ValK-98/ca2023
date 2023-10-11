a82b7edf34ea16c110afef0321f58904fdc038cb
ValK-98 <vkornijenka@gmail.com>
2023-10-11 13:15:48 +1000

    module notes 2

--- python/data-structures/main.py
+++ python/data-structures/main.py
@@ -1,4 +1,23 @@
-import my_module
+# import my_module
+# imports whole module 
 
-print(my_module.s)
+from my_module import foo as bar, person, x
+# imports specific modules
+# if you sufix with as it removed conflict
 
+# print(my_module)
+# prints location
+
+# print(dir(my_module))
+# lists modules
+
+def foo(x):
+    print(x)
+
+foo(person)
+bar({'name': 'Matt', 'age': 51})
+
+# don't need to prefix when importing specific modules
+
+
+print(dir())
