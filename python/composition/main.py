99a4fdd97a3d0b23f763acc489f19ce4d77195a7
ValK-98 <vkornijenka@gmail.com>
2023-10-16 19:35:29 +1000

    finished composition lesson

--- python/composition/main.py
+++ python/composition/main.py
@@ -5,14 +5,18 @@ aragorn = rpg.Character('Aragorn', 'Human')
 galadriel = rpg.Character('Galadriel', 'Elf')
 frodo = rpg.Character('Frodo', 'Hobbit')
 
-frodo.set_currency(9, 47, 23)
+frodo.inv.set_currency(9, 47, 23)
 
 chest = rpg.Chest(['longsword', 'iron helm'], 2, 25, 50)
 
 
-print(chest.__dict__)
+# print(chest.inv.__dict__)
 # print(aragorn.__dict__)
 # print(frodo.__dict__)
 # print(galadriel.__dict__)
 
-print(frodo.get_currency())
+chest.inv.transfer(frodo.inv)
+
+print(frodo.inv.__dict__)
+print(frodo.inv.get_currency())
+print(chest.inv.__dict__)
--- python/composition/rpg.py
+++ python/composition/rpg.py
@@ -3,8 +3,23 @@ class Character:
         self.name = name 
         self.race = race
         self.copper = 0
+        self.inv= Inventory([], 0, 0, 0)
+
+class Chest:
+    def __init__(self, items, gold, silver, copper):
+        self.inv = Inventory(items, gold, silver, copper)
+class Inventory: 
+    def __init__(self, items, gold, silver, copper):
+        self.items = items
+        self.set_currency(gold, silver, copper)
     
-    # Setter
+    def transfer(self, to_inv):
+        to_inv.items.extend(self.items)
+        self.items = []
+        to_inv.copper += self.copper
+        self.copper = 0
+
+        # Setter
     def set_currency(self, gold, silver, copper):
         self.copper = gold * 10000 + silver * 100 + copper
 
@@ -14,10 +29,3 @@ class Character:
         silver = (self.copper % 10000) // 100
         copper = self.copper % 100
         return gold, silver, copper
-
-class Chest:
-    def __init__(self, items, gold, silver, copper):
-        self.items = items
-        self.gold = gold
-        self.silver = silver
-        self.copper = copper
