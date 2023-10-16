55e79a11a7dc86f2ea4a7e216075968484cf71b1
ValK-98 <vkornijenka@gmail.com>
2023-10-16 11:06:29 +1000

    init commit

---
+++ python/composition/rpg.py
@@ -0,0 +1,23 @@
+class Character: 
+    def __init__(self, name, race): 
+        self.name = name 
+        self.race = race
+        self.copper = 0
+    
+    # Setter
+    def set_currency(self, gold, silver, copper):
+        self.copper = gold * 10000 + silver * 100 + copper
+
+    # Getter
+    def get_currency(self):
+        gold = self.copper // 10000
+        silver = (self.copper % 10000) // 100
+        copper = self.copper % 100
+        return gold, silver, copper
+
+class Chest:
+    def __init__(self, items, gold, silver, copper):
+        self.items = items
+        self.gold = gold
+        self.silver = silver
+        self.copper = copper
