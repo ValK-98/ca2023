9c20b38b7142c8ec9dd751e2f138ba179fb04d44
ValK-98 <vkornijenka@gmail.com>
2023-10-19 13:50:59 +1000

    course notes

--- python/composition/rpg.py
+++ python/composition/rpg.py
@@ -1,55 +1,67 @@
-class Character: 
-    def __init__(self, name, race, health, attack): 
-        self.name = name 
+class Character:
+    def __init__(self, name, race, health, attack):
+        self.name = name
         self.race = race
-        self.health = health 
+        self.health = health
         self.attack = attack
-        self.inv= Inventory([], 0, 0, 0)
-    
+        self.inv = Inventory([], 0, 0, 0)
+
     def battle(self, other):
-                print(f'{self.name} attacks {other.name}!')
+        print(f'{self.name} attacks {other.name}!')
+
+
+class Ranger(Character):
+    def battle(self, other):
+        print(f'{self.name} launches a brutal melee attack on {other.name}!')
+
+    def recruit_undead(self):
+        pass
+
 
 class Mage(Character):
-       def __init__(self, name, race, health, attack): 
+    def __init__(self, name, race, health, attack):
+        # Call the superclass constructor
         super().__init__(name, race, health, attack)
         self.mana = 100
-       
-
-       def battle(self, other):
-                print(f'{self.name} casts a wicked spell at {other.name}!')
-                self.mana -= 20
+        
+    def battle(self, other):
+        print(f'{self.name} casts a wicked spell at {other.name}!')
+        self.mana -= 20
 
 
 class Burglar(Character):
-       def battle(self, other):
-                print(f'{self.name} sneaks in a stealth attack on {other.name}!')
+    pass
+    # def battle(self, other):
+    #     print(f'{self.name} sneaks in a stealth attack on {other.name}!')
+
 
 class Wizard(Character):
-       def battle(self, other):
-                print(f'{self.name} summons an orc minion, which attacks {other.name}!')
+    def battle(self, other):
+        print(f'{self.name} summons an orc minion, which attacks {other.name}!')
 
-class Ranger(Character):
-       def battle(self, other):
-                print(f'{self.name} launches a brutal melee attack on{other.name}!')
 
-       def recruit_undead(self):
-             pass  
 
 class Chest:
     def __init__(self, items, gold, silver, copper):
         self.inv = Inventory(items, gold, silver, copper)
-class Inventory: 
+
+class Inventory:
     def __init__(self, items, gold, silver, copper):
-        self.items = items
-        self.set_currency(gold, silver, copper)
-    
+        self.items = items # list
+        self.set_currency(gold, silver, copper) # Delegation
+
+    # from_inv and to_inv are instances of Inventory
     def transfer(self, to_inv):
+        # Add all the items from from_inv to to_inv
         to_inv.items.extend(self.items)
+        # Delete all the items from from_inv
         self.items = []
+        # Add the currency from from_inv to to_inv
         to_inv.copper += self.copper
+        # Set currency of from_inv to 0
         self.copper = 0
 
-        # Setter
+    # Setter
     def set_currency(self, gold, silver, copper):
         self.copper = gold * 10000 + silver * 100 + copper
 
--- python/fcc_tutorial/app.py
+++ python/fcc_tutorial/app.py
@@ -1,5 +1,26 @@
 
-def cube(num):
-    num*num*num
+# def raise_to_power(base_num, pow_num):
+#     result = 1
+#     for index in range(pow_num):
+#         result = result * base_num
+#     return result
 
-cube(3)
+# print(raise_to_power(3,4))
+
+def risiko(attacker, defender):
+    attacker.sort(reverse=True)
+    defender.sort(reverse=True)
+
+    num_comparisons = min(len(attacker), len(defender))
+    
+    armies_lost = 0
+    
+    for i in range(num_comparisons):
+        if attacker[i] > defender[i]:
+            armies_lost += 1
+    
+    return armies_lost
+
+print(risiko([3, 6, 4], [2, 5, 3]))  
+print(risiko([3, 6], [6, 4, 4]))     
+print(risiko([3, 1], [1]))            
