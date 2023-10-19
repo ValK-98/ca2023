ac58c8c7f5bf66d20272cacc6c5a7a4e21143c22
ValK-98 <vkornijenka@gmail.com>
2023-10-19 13:50:41 +1000

    finished masterclass

---
+++ python/masterclass/lambdas.py
@@ -0,0 +1,42 @@
+nums = [10, 14, 21, 50, 5, -6]
+
+# def square(value):
+#     return value ** 2
+
+# def cube(value):
+#     return value ** 3
+
+# square = lambda x: x ** 2
+
+# print(square(10))
+
+# squared_nums = []
+# for num in nums:
+#     squared_nums.append(square(num))
+
+
+# squared_nums = list(map(lambda num: num ** 2, nums))
+
+# squared_nums = [num ** 2 for num in nums if num % 2 == 0]
+
+# even_numbers = list(filter(lambda num: num % 2 == 0, nums))
+
+# print(squared_nums)
+# print(even_numbers)
+
+
+# employees = [
+#     { 'name': 'John', 'age': 32},
+#     { 'name': 'Mary', 'age': 27},
+#     { 'name': 'Bob', 'age': 40},
+# ]
+
+# sorted_employees = sorted(employees, key=lambda x: x['name'])
+# oldest_employee = max(employees, key=lambda x: x['age'])
+# print(sorted_employees)
+# print(oldest_employee)
+
+
+students = [
+    {}
+]
--- python/masterclass/main.py
+++ python/masterclass/main.py
@@ -10,10 +10,37 @@ def my_decorator(spam, num):
 
     return inner_function
 
-@my_decorator(num=42) 
-def greet(name):
-    print(f'Hello, {name}!')
+# @my_decorator(num=42) 
+# def greet(name):
+#     print(f'Hello, {name}!')
 
 
-greet()
+# greet()
 # my_function()
+
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, other): 
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __eq__ (self, other) -> bool:
+        return self.x == other.x and self.y == other.y
+    
+    def __repr__(self) -> str:
+        return f'Point({self.x}, {self.y})'
+    
+    def __str__(self) -> str:
+        return f' x: {self.x}, y: {self.y}'
+
+p1 = Point(1,2)
+p2 = Point(2,3)
+
+print(p1 == p2)
+
+p1 += p2 
+print(repr(p1))
+
+# print((p1 + p2).__dict__)
