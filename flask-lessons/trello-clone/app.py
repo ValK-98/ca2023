c54de6d6d6116945758e42b8d4b079a12371923f
ValK-98 <vkornijenka@gmail.com>
2023-11-15 19:36:20 +1000

    finished lesson

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,5 +1,6 @@
-from flask import Flask 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
@@ -8,16 +9,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:nahui@
 db = SQLAlchemy(app)
 
 class Card(db.Model):
+    __tablename__ = 'cards'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    description = db.Column(db.text())
+    description = db.Column(db.Text())
     date_created = db.Column(db.Date())
 
 
 @app.cli.command('db_create')
 def db_create():
+    db.drop_all()
     db.create_all()
     print('Created tables')
+
+@app.cli.command('db_seed')
+def db_seed():
+    card = Card(
+        title = 'Start the project',
+        description = 'Stage 1 - Create ERD',
+        date_created = date.today(),
+    )
+
+    db.session.add(card)
+    db.session.commit()
+
+    print('Database seeded')
 
 
 @app.route('/')
