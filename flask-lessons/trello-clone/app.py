498383de502969f1a49c63bcf4f34825701d8b31
ValK-98 <vkornijenka@gmail.com>
2023-11-22 15:54:57 +1000

    finished lesson

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -4,14 +4,19 @@ from datetime import date
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from datetime import timedelta
 
 app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = 'Ministry of Silly Walks'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:nahui@localhost:5432/trello'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 
 class Card(db.Model):
@@ -122,18 +127,19 @@ def login():
     stmt = db.select(User).where(User.email == user_info['email'])
     user = db.session.scalar(stmt)
     # Check password has
-    if bcrypt.check_password_hash(user.password, user_info['password']):
-        return UserSchema(exclude=['password']).dump(user)
+    if user and bcrypt.check_password_hash(user.password, user_info['password']):
+        # Create a JWT Token 
+        token = create_access_token(identity=user.email, expires_delta=timedelta(hours=2))
+        # Send the token to client
+        return {'token': token, 'user': UserSchema(exclude=['password']).dump(user)}
     else:
         return {'error': 'Invalid email or password'}, 401
-    # Create a JWT Token 
     
-    # Send the token to client
-    return 'ok'
 
 
 
 @app.route('/cards')
+@jwt_required()
 def all_cards():
     # select * from cards;
     stmt = db.select(Card) #.where(db.or_(Card.status != 'Done', Card.id > 2)).order_by(Card.title.desc())
