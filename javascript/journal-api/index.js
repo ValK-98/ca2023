348bf0a0758a90589928f4e9afbb4f64fc57b9e2
ValK-98 <vkornijenka@gmail.com>
2024-01-29 09:18:55 +1000

    up to date

--- javascript/journal-api/index.js
+++ javascript/journal-api/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
-import { EntryModel, CategoryModel } from './db.js'
-
-const categories = ['Food', 'Gaming', 'Coding', 'Other']
+import { CategoryModel } from './db.js'
+import entryRoutes from './routes/entry_routes.js'
 
 const app = express()
 
@@ -9,63 +8,12 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send({ info: 'Journal API' }))
 
+// TODO: Move /categories to routes folder
+// TODO: Complete categories CRUD
+// TODO: ADVANCED: Modify "GET /categories/:id" to embed an array of all the entries in that category
+
 app.get('/categories', async (req, res) => res.send(await CategoryModel.find()))
 
-app.get('/entries', async (req, res) => res.send(await EntryModel.find()))
-
-app.get('/entries/:id', async (req, res) => {
-    const entry = await EntryModel.findById(req.params.id)
-    if (entry) {
-        res.send(entry)
-    } else {
-        res.status(404).send({ error: 'Entry not found' })
-    }
-})
-
-app.post('/entries', async (req, res) => {
-    try {
-        // Get entry data from the request
-        // console.log(req.body)
-        // TODO: Validate
-        // Create a new entry object
-        // Push the new entry to the array
-        // entries.push(req.body)
-        const insertedEntry = await EntryModel.create(req.body)
-        // Respond with 201 and the created entry
-        res.status(201).send(insertedEntry)
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
-
-app.put('/entries/:id', async (req, res) => {
-    try {
-        const updatedEntry = await EntryModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        if (updatedEntry) {
-            res.send(updatedEntry)
-        } else {
-            res.status(404).send({ error: 'Entry not found' })
-        }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
-
-app.delete('/entries/:id', async (req, res) => {
-    try {
-        const deletedEntry = await EntryModel.findByIdAndDelete(req.params.id)
-        if (deletedEntry) {
-            res.sendStatus(204)
-        } else {
-            res.status(404).send({ error: 'Entry not found' })
-        }
-    }
-    catch (err) {
-        res.status(500).send({ error: err.message })
-    }
-})
-
+app.use('/entries', entryRoutes)
 
 app.listen(8003)
